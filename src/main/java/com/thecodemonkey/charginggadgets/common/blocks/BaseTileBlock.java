02594c719094b295f9dfda499fb42a9b37afebe8
The-Code-Monkey <andy.wilson1234@gmail.com>
2020-08-19 10:39:56 +0000

    fix restart building process

---
+++ src/main/java/com/thecodemonkey/charginggadgets/common/blocks/BaseTileBlock.java
@@ -0,0 +1,5 @@
+package com.thecodemonkey.charginggadgets.common.blocks;
+
+public abstract class BaseTileBlock<T extends BaseBlockEntity> extends BaseBlock implements BlockEntityProvider, AttributeProvider {
+    
+}
