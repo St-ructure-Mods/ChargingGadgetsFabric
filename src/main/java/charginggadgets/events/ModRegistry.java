24cea791e283c602ecb52d9541554ade58fba511
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2020-08-21 22:37:28 +0100

    Update ModRegistry.java

--- src/main/java/charginggadgets/events/ModRegistry.java
+++ src/main/java/charginggadgets/events/ModRegistry.java
@@ -1,7 +1,11 @@
 package charginggadgets.events;
 
 import charginggadgets.ChargingGadgets;
+import chargingadgets.init.CGContent.*
 import net.minecraft.item.Item;
+import reborncore.RebornRegistry;
+
+import java.util.Arrays;
 
 public class ModRegistry {
 
@@ -13,6 +17,7 @@ public class ModRegistry {
     private static void registerBlocks() {
         Item.Settings itemGroup = new Item.Settings().group(ChargingGadgets.ITEMGROUP);
 
+        Arrays.stream(Machine.values()).forEach(value -> RebornRegistry.registerBlock(value.block, itemGroup));
     }
 
     private static void registerItems() {
